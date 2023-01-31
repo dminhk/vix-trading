5e743302470e71faf50840cb0dcf43be9adbc413
Hyoung Kyu Min <dminhk@gmail.com>
2023-01-31 01:29:49 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+from AlgorithmImports import *
+
+class VixBasedInvesting(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2020, 1, 1)  # Set Start Date
+        self.SetEndDate(2023, 1, 1)  # Set Start Date
+        self.SetCash(10000)  # Set Strategy Cash
+        self.spy = self.AddEquity("SPY", Resolution.Daily)
+        self.vix = self.AddData(CBOE, "VIX").Symbol
+        self.invest = True
+        #--------------------------------------------------------------
+        self.VIXbuy = 20
+        self.VIXsell = 30
+        #--------------------------------------------------------------
+
+
+
+    def OnData(self,data):
+        if not self.Portfolio.Invested and self.Securities[self.vix].Price > self.VIXbuy:
+            self.SetHoldings("SPY", 1)
+
+        closing_price = self.Portfolio["SPY"].Price
+
+        if self.Securities[self.vix].Price < self.VIXsell:
+            self.Liquidate("SPY")
+            self.invest = False
